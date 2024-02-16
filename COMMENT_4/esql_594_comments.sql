5aa78f58fda6fb71b43b8585914eda33ef11af49
Visual Builder Studio Builder <devcs-builder@oracle.com>
2024-02-16 12:01:36 +0000

    new code

---
+++ COMMENT_4/esql_594_comments.sql
@@ -0,0 +1,2 @@
+
+   COMMENT ON TABLE "ESQL_594"  IS 'Data Pump Master Table EXPORT                         SCHEMA                        ';
---
+++ f102_5/application/shared_components/user_interface/lovs/p68_product.sql
@@ -0,0 +1,27 @@
+prompt --application/shared_components/user_interface/lovs/p68_product
+begin
+--   Manifest
+--     P68_PRODUCT
+--   Manifest End
+wwv_flow_imp.component_begin (
+ p_version_yyyy_mm_dd=>'2023.10.31'
+,p_release=>'23.2.3'
+,p_default_workspace_id=>20
+,p_default_application_id=>7170
+,p_default_id_offset=>7728180845834644
+,p_default_owner=>'ORACLE'
+);
+wwv_flow_imp_shared.create_list_of_values(
+ p_id=>wwv_flow_imp.id(16593967885260269129)
+,p_lov_name=>'P68_PRODUCT'
+,p_lov_query=>wwv_flow_string.join(wwv_flow_t_varchar2(
+'select product_name d, id r',
+'from   eba_cust_products',
+'where id = :P68_PRODUCT_ID',
+'order by 1'))
+,p_source_type=>'LEGACY_SQL'
+,p_location=>'LOCAL'
+);
+wwv_flow_imp.component_end;
+end;
+/
